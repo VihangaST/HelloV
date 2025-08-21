3cc03c230bd28da663dfe4b3cffd9eb8fc1bf920
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-21 10:39:51 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Settings from "./Pages/Settings";
 import Grid from "./components/Grid";
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Grid />
     </>
   );
