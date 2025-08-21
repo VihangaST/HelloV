43114c02636763d564a10918f2f2d0feaac12c2e
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-21 10:04:35 +0530

    added card, Grid components, import Boostrap JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import Settings from "./Pages/Settings";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-      </div>
-      <h1>HelloV</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-      Feel the artistic vibe
-      </p>
+      <Settings />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
