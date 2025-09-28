f3c1bf30b851029079b7429edf06fa7539af435e
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-09-28 18:22:48 +0530

    card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,14 +28,14 @@ function Card({ carouselId, card }) {
           <div className="carousel-inner">
             <div className="carousel-item active">
               <img
-                src={`/public/assets/BD/${card.images}-1.jpg`}
+                src={`/assets/BD/${card.images}-1.jpg`}
                 className="d-block w-100"
                 alt="..."
               />
             </div>
             <div className="carousel-item">
               <img
-                src={`public/assets/BD/${card.images}-2.jpg`}
+                src={`/assets/BD/${card.images}-2.jpg`}
                 className="d-block w-100"
                 alt="..."
               />
