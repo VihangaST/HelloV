7a50ba19922d23b40bb5503b8a0e2bcd4c04f03e
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-09-28 14:07:11 +0530

    bug fix- video upload

--- src/Pages/Video.jsx
+++ src/Pages/Video.jsx
@@ -5,7 +5,7 @@ export default function Video() {
         <video
           className="background-video"
           style={{ width: "100%", height: "400px", objectFit: "cover" }}
-          src="public/assets/BD/video_1.mp4"
+          src="/assets/BD/video_1.mp4"
           autoPlay
           loop
           muted
